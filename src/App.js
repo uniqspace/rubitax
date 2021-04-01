3b9033eb8d056c2803b9df81fe68e86740646767
Alexey Kot <alexeykot97@gmail.com>
2021-04-01 20:40:54 +0300

    minor style fixes

--- src/App.js
+++ src/App.js
@@ -7,18 +7,16 @@ import {
 } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-// import Container from '@material-ui/core/Container';
-// import Topbar from './components/Topbar';
-// import Sidebar from './components/Sidebar';
+import Container from '@material-ui/core/Container';
+import Topbar from './components/Topbar';
+import Sidebar from './components/Sidebar';
 
 import Onboarding from './container/Onboarding/Onboarding';
-import Login from './container/Auth/Login';
-import Forgot from './container/Auth/Forgot';
 
 import './assets/font/Lato/lato.css';
 import './assets/css/style.css';
-
-const authenticated = true;
+import Login from './container/Auth/Login';
+import Forgot from './container/Auth/Forgot';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,9 +25,8 @@ const useStyles = makeStyles((theme) => ({
   content: {
     flexGrow: 1,
     overflow: 'auto',
-    marginTop: !authenticated ? 88 : 0,
-    minHeight: 'calc(100vh - 88px)',
-    backgroundColor: 'white',
+    marginTop: 88,
+    minHeight: 'calc(100vh - 88px)'
   },
   container: {
     padding: '20px 12px',
@@ -46,41 +43,37 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function App() {
-  const classes = useStyles();
+  // const classes = useStyles();
   // const [mobileOpen, setMobileOpen] = React.useState(false);
   // const handleDrawerToggle = () => {
   //   setMobileOpen(!mobileOpen);
   // };
-
   return (
-    <>
-    {/* <div className={classes.root}> */}
-      <CssBaseline />
-      {/* {!authenticated &&
-      <>
-        <Topbar
-          mobileOpen={mobileOpen}
-          handleDrawerToggle={handleDrawerToggle}
-        />
-        <Sidebar
-          mobileOpen={mobileOpen}
-          handleDrawerToggle={handleDrawerToggle}
-        />
-      </>
-      } */}
-      {/* <main className={classes.content}>
-        <Container maxWidth="lg" className={!authenticated ?  classes.container : ""}> */}
+        // <div className={classes.root}>
+    //   <CssBaseline />
+    //   <Topbar
+    //     mobileOpen={mobileOpen}
+    //     handleDrawerToggle={handleDrawerToggle}
+    //   />
+    //   <Sidebar
+    //     mobileOpen={mobileOpen}
+    //     handleDrawerToggle={handleDrawerToggle}
+    //   />
+    //   <main className={classes.content}>
+    //     <Container maxWidth="lg" className={classes.container}>
+        <>
+          <CssBaseline />
           <Router>
             <Switch>
-              <Redirect exact from="/" to={{pathname: '/login'}} />
+              <Redirect exact from="/" to={{pathname: '/onboarding'}} />
               <Route exact path="/onboarding" component={Onboarding} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/forgot" component={Forgot} />
             </Switch>
           </Router>
-        {/* </Container>
-      </main> */}
-    {/* </div> */}
-    </>
+        </>
+    //     </Container>
+    //   </main>
+    // </div>
   );
 }
--- src/container/Onboarding/Onboarding.js
+++ src/container/Onboarding/Onboarding.js
@@ -4,6 +4,7 @@ import { fade } from '@material-ui/core/styles/colorManipulator';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import TabPanel from '../../components/TabPanel';
 
@@ -16,18 +17,20 @@ import Topbar from '../../components/Topbar';
 import Sidebar from '../../components/Sidebar';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    padding: '20px 12px',
-    [theme.breakpoints.up('sm')]: {
-      padding: '25px 36px',
-    },
+  root: {
+    display: 'flex',
   },
   content: {
     flexGrow: 1,
     overflow: 'auto',
     marginTop: 88,
-    minHeight: 'calc(100vh - 88px)',
-    backgroundColor: 'white',
+    minHeight: 'calc(100vh - 88px)'
+  },
+  container: {
+    padding: '20px 12px',
+    [theme.breakpoints.up('sm')]: {
+      padding: '35px 36px 25px 36px',
+    },
   },
   paper: {
     padding: '12px 24px',
@@ -87,48 +90,49 @@ function Onboarding(props) {
   }
 
   return (
-    <div style={{display: 'flex'}}>
-    <Topbar
-      mobileOpen={mobileOpen}
-      handleDrawerToggle={handleDrawerToggle}
-    />
-    <Sidebar
-      mobileOpen={mobileOpen}
-      handleDrawerToggle={handleDrawerToggle}
-    />
-    <main className={classes.content}>
-    <Container maxWidth="lg" className={classes.container}>
-      <OnBoardingContextProvider>
-        <Paper className={classes.paper}>
-          <Tabs
-            value={activedTab}
-            indicatorColor="primary"
-            textColor="primary"
-            onChange={handleChangeTab}
-            variant="fullWidth"
-          >
-            <Tab
-              className={classes.tabHeader}
-              label="STEP 1"
-            />
-            <Tab
-              className={classes.tabHeader}
-              label="STEP 2"
-              disabled={!formSubmitted}
-            />
-          </Tabs>
-          <TabPanel value={activedTab} index={0}>
-            <OnboardingStep1
-              moveStepTwo={moveStepTwo}
-            />
-          </TabPanel>
-          <TabPanel value={activedTab} index={1}>
-            <OnboardingStep2 />
-          </TabPanel>
-        </Paper>
-      </OnBoardingContextProvider>
-      </Container>
-    </main>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Topbar
+        mobileOpen={mobileOpen}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+      <Sidebar
+        mobileOpen={mobileOpen}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+      <main className={classes.content}>
+        <Container maxWidth="lg" className={classes.container}>
+        <OnBoardingContextProvider>
+          <Paper className={classes.paper}>
+            <Tabs
+              value={activedTab}
+              indicatorColor="primary"
+              textColor="primary"
+              onChange={handleChangeTab}
+              variant="fullWidth"
+            >
+              <Tab
+                className={classes.tabHeader}
+                label="STEP 1"
+              />
+              <Tab
+                className={classes.tabHeader}
+                label="STEP 2"
+                disabled={!formSubmitted}
+              />
+            </Tabs>
+            <TabPanel value={activedTab} index={0}>
+              <OnboardingStep1
+                moveStepTwo={moveStepTwo}
+              />
+            </TabPanel>
+            <TabPanel value={activedTab} index={1}>
+              <OnboardingStep2 />
+            </TabPanel>
+          </Paper>
+        </OnBoardingContextProvider>
+        </Container>
+      </main>
     </div>
   );
 }
